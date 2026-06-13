1a3a4ce2c3456ef9c90cc6a92510eaf0788c05a0
Jhd1006 <doctor1006123@gmail.com>
2026-06-13 21:37:38 +0900

    [성공]

---
+++ programmers/정렬/가장 큰 수.py
@@ -0,0 +1,27 @@
+# from functools import cmp_to_key
+
+# def func(a, b):
+#     if a + b > b + a:
+#         return -1
+#     elif a + b < b + a:
+#         return 1
+#     return 0
+
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     sorted_numbers = sorted(numbers, key = cmp_to_key(func))
+#     answer = ''
+#     answer= ''.join(sorted_numbers)
+#     if answer[0] == '0':
+#         return '0'
+#     return answer
+
+# 다른 풀이
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    numbers.sort(key = lambda x : (x * 4), reverse = True)
+    answer = ''.join(numbers)
+    if answer[0] == '0':
+        return '0'
+    return answer
+    return answer
