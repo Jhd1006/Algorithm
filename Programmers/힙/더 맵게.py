0ea830e08b69fcb809e2bd09a2e1d1ef59efcecf
Jhd1006 <doctor1006123@gmail.com>
2026-06-13 21:17:31 +0900

    [성공]

---
+++ Programmers/힙/더 맵게.py
@@ -0,0 +1,17 @@
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)  
+    answer = 0
+    while scoville:
+        if scoville[0] >= K:
+            break
+        if len(scoville) < 2:
+            return -1
+        answer += 1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        mixed = first + second * 2
+        heapq.heappush(scoville, mixed)
+    return answer
+print(solution([1, 2, 3, 9, 10, 12], 7))
