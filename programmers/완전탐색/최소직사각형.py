4bad584192b319ff108c77d24279cb1ef3e570aa
Jhd1006 <doctor1006123@gmail.com>
2026-06-14 21:30:07 +0900

    Delete programmers/완전탐색 directory

--- programmers/완전탐색/최소직사각형.py
+++
@@ -1,9 +0,0 @@
-def solution(sizes):
-    answer = 0
-    x, y = 0, 0
-    for s in sizes:
-        w, h = max(s), min(s)
-        x = max(x, w)
-        y = max(y, h)
-    answer = x * y
-    return answer
