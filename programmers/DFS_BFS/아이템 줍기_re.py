719ad01d8014aee716a8c7e7c13bdb3c1e1d356e
Jhd1006 <doctor1006123@gmail.com>
2026-07-18 18:24:19 +0900

    [성공]

---
+++ programmers/DFS_BFS/아이템 줍기_re.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+def solution(rectangle, characterX, characterY, itemX, itemY):
+    answer = 0
+    dx = [-1, 0, 1, 0]
+    dy = [0, -1, 0, 1]
+    board = [[0] * 102 for _ in range(102)]
+    dist = [[-1] * 102 for _ in range(102)]
+    for x1, y1, x2, y2 in rectangle:
+        x1 *= 2
+        y1 *= 2
+        x2 *= 2
+        y2 *= 2
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                board[x][y] = 1
+    for x1, y1, x2, y2 in rectangle:
+        x1 *= 2
+        y1 *= 2
+        x2 *= 2
+        y2 *= 2
+        for x in range(x1 + 1, x2):
+            for y in range(y1 + 1, y2):
+                board[x][y] = 0
+    characterX *= 2
+    characterY *= 2
+    itemX *= 2
+    itemY *= 2
+    q = deque([(characterX, characterY)])
+    dist[characterX][characterY] = 0
+    while q:
+        x, y = q.popleft()
+        if x == itemX and y == itemY:
+            return dist[x][y] // 2
+        for dir in range(4):
+            nx = x + dx[dir]
+            ny = y + dy[dir]
+            if nx < 0 or ny < 0 or nx >= 102 or ny >= 102:
+                continue
+            if board[nx][ny] != 1 or dist[nx][ny] != -1:
+                continue
+            dist[nx][ny] = dist[x][y] + 1
+            q.append((nx, ny))
+    return answer
