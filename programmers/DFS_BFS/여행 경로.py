d37557f875230000fcd333e5790ac0feec323c8a
Jhd1006 <doctor1006123@gmail.com>
2026-07-18 19:13:15 +0900

    [성공] - 권장 코드 추가
    불필요한 탐색 방지를 위한 조기종료 조건 추가

--- programmers/DFS_BFS/여행 경로.py
+++ programmers/DFS_BFS/여행 경로.py
@@ -17,3 +17,23 @@ def solution(tickets):
     answer = answer[0]
     return answer
 
+def solution(tickets):
+    answer = []
+    l = len(tickets)
+    tickets.sort()
+    vis = [False] * l
+    def dfs(cur, path):
+        if len(path) == l + 1:
+            answer.append(path) 
+            return True
+        for idx, ticket in enumerate(tickets):
+            if vis[idx] or ticket[0] != cur:
+                continue
+            vis[idx] = True
+            if dfs(ticket[1], path + [ticket[1]]):
+                return True
+            vis[idx] = False
+        return False
+    dfs('ICN', ['ICN'])
+    answer = answer[0]
+    return answer
