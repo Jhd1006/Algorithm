f8deb75e4a9591566efad56783ef9419f127fc84
Jhd1006 <doctor1006123@gmail.com>
2026-07-18 19:30:45 +0900

    [성공] - 권장 코드 추가
    불필요한 탐색 방지를 위한 조기 종료 조건 추가

--- programmers/DFS_BFS/여행 경로.py
+++ programmers/DFS_BFS/여행 경로.py
@@ -30,10 +30,9 @@ def solution(tickets):
             if vis[idx] or ticket[0] != cur:
                 continue
             vis[idx] = True
-            if dfs(ticket[1], path + [ticket[1]]):
+            if dfs(ticket[1], path + [ticket[1]]):         # 더 깊은 곳의 dfs가 True를 반환하면, dfs 연쇄적으로 True 반환하며 조기 종료
                 return True
             vis[idx] = False
-        return False
     dfs('ICN', ['ICN'])
     answer = answer[0]
     return answer
